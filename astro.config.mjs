32ce2bd7582d8250d027ea896238bb7a391353f2
Keith Robinson <kthrob@gmail.com>
2025-09-05 19:44:38 -0500

    upgrade to tailwindcss v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,25 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
+// import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
 import icon from 'astro-icon';
+import { fileURLToPath } from "url";
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   output: 'static',
   site: "https://kthrob.github.io",
-  integrations: [tailwind(), icon(), mdx(), sitemap()]
+  integrations: [icon(), mdx(), sitemap()],
+  vite: {
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "./src"),
+      },
+    },
+    plugins: [ tailwindcss() ],
+  },
 });
